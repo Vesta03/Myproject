a5a08d451ee968a6aea17ab5ab007dd4fd93fbd3
Khamit <hamitik123@mail.ru>
2019-11-22 17:59:42 +0300

    Added new project

---
+++ My project Mac/Assets/clsnentr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clsnentr : MonoBehaviour
+{
+    public time Customer;
+    private void OnCollisionEnter(Collision collision)
+    {
+       if(collision.transform.name == "Plate")
+        {
+            Destroy(Customer);
+        }
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project Mac/Assets/time.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class time : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float timer;
+    public GameObject Customer;
+         
+
+    void Start()
+    {
+        timer = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            Destroy(Customer);
+        }
+    }
+}
